96a14ca8d49a5164ce18810fe652a5c0086b9db0
Jay <boredom_slaloms.0q@icloud.com>
2025-05-12 18:14:41 -0400

    Update CertificatesView.swift
    certflip1

--- Feather/Views/Settings/Certificates/CertificatesView.swift
+++ Feather/Views/Settings/Certificates/CertificatesView.swift
@@ -18,7 +18,7 @@ struct CertificatesView: View {
 	// MARK: Fetch
 	@FetchRequest(
 		entity: CertificatePair.entity(),
-		sortDescriptors: [NSSortDescriptor(keyPath: \CertificatePair.date, ascending: false)],
+		sortDescriptors: [NSSortDescriptor(keyPath: \CertificatePair.date, ascending: true)],
 		animation: .snappy
 	) private var certificates: FetchedResults<CertificatePair>
 	
